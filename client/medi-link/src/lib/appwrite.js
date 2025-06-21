12731b254bb6f26a3da7318422b2bff01a6b75f4
JadhavAnsh <jadhavansh10@gmail.com>
2025-06-21 13:37:53 +0530

    refactor(appwrite): clean up client initialization and remove unused databases import

--- client/medi-link/src/lib/appwrite.js
+++ client/medi-link/src/lib/appwrite.js
@@ -1,20 +1,15 @@
-// src/lib/appwrite.js
-import { Client, Databases, ID, Storage } from "appwrite";
+'use client';
+
+import { Client, ID, Storage } from "appwrite";
 
 const client = new Client();
 
-client
-  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID)
+if (typeof window !== "undefined") {
+  client
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "")
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || "");
+}
 
-// client.headers['X-Appwrite-Key'] = process.env.APPWRITE_API_KEY;
-
-const databases = new Databases(client);
 const storage = new Storage(client);
 
-// client.ping = async () => {
-//   return await databases.listDocuments(process.env.NEXT_PUBLIC_API_APPWRITE_DATABASE_ID);
-// };
-
-export { client, databases, ID, storage };
-
+export { client, ID, storage };
